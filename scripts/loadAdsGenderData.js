3b0d4f348eae9e23b5dd756fbde7f2cdb3e46ade
Marcio <marciodosanjos@ymail.com>
2024-10-21 05:10:54 -0300

    fix importing model AdsGenderData

--- scripts/loadAdsGenderData.js
+++ scripts/loadAdsGenderData.js
@@ -1,5 +1,5 @@
 const dbConnect = require("../config/dbConnect");
-const AdsGenderData = require("../model/AdsGenderData.js");
+const AdsGenderData = require("../Model/AdsGenderData.js");
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const { JWT } = require("google-auth-library");
 const express = require("express");
